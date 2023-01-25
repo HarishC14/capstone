1c3cfa49ad0cace8c4b10631160288a656721c9d
EC2 Default User <ec2-user@ip-172-31-63-202.ec2.internal>
2023-01-25 07:23:42 +0000

    added

---
+++ build/precache-manifest.c58c96a99f3e787d2320b5d33bad377c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542a72626ac1978e0cc7b5e1e10bdca2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9549f82aad398b974bee",
+    "url": "/static/css/2.741e3868.chunk.css"
+  },
+  {
+    "revision": "b3c08482466d8869a4b6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9549f82aad398b974bee",
+    "url": "/static/js/2.d24f8170.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d24f8170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c08482466d8869a4b6",
+    "url": "/static/js/main.87d86439.chunk.js"
+  },
+  {
+    "revision": "c1495edec51ec84df206",
+    "url": "/static/js/runtime-main.9305fcc0.js"
+  }
+]);
